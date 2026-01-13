e298e4b422e2434d25e7a4d155397822ec1e32b1
LuisQuezada7 <luis.quezada1701@alumnos.ubiobio.cl>
2026-01-13 18:49:49 -0300

    Configuracion global de axios

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { CartProvider } from './context/CartContext' // <--- IMPORTAR
+import { CartProvider } from './context/CartContext'
+import axios from 'axios' // <--- 1. NUEVO: IMPORTAR AXIOS
+
+// <--- 2. NUEVO: CONFIGURACIÓN GLOBAL DE URL
+// Si existe la variable en Netlify (VITE_API_URL), usa esa.
+// Si no existe (estás en tu PC), usa localhost:5000.
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CartProvider> {/* <--- ENVOLVER INICIO */}
+    <CartProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </CartProvider> {/* <--- ENVOLVER FINAL */}
+    </CartProvider>
   </React.StrictMode>,
 )
